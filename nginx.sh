9d675c8aba99449e8f8ac93a396b451ac71dc4de
root <root@dick.fvds.ru>
2020-01-01 14:29:16 -0500

    sites-enabled to sites-available

--- nginx.sh
+++ nginx.sh
@@ -2,5 +2,5 @@
 #Install nginx
 apt-get update
 sudo apt install nginx -y
-sudo yes | sudo cp -i sites-enabled/sample /etc/nginx/sites-enabled/
+sudo yes | sudo cp -i sites-enabled/sample /etc/nginx/sites-available/
 sudo systemctl restart nginx
